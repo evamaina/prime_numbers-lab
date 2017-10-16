a9ddd14a349b57d2c788c806670ba2502ebf670a
Eva <evajohnson714@gmail.com>
2017-10-16 18:41:23 +0300

    Test for the prime numbers

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,22 @@
+def primeNumbers(n):
+	prime_numbers = []
+	if isinstance(n,list):
+		raise TypeError("Input cannot be a list")
+	elif isinstance(n,dict):
+		raise TypeError("Input cannot be a dictionary")
+	elif n < 1:
+		return []
+	else:
+		for num in range(2,n):
+			for i in range(2,num):
+				if (num%i) == 0:
+					break
+			else:
+				prime_numbers.append(num)
+	return prime_numbers
+
+	
+	print (primeNumbers(15))
+
+
 
